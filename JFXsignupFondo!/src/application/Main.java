28351c510aa19bed27e7ffc8171d80abdbebbf92
Gonzalo <gonzalo224455@gmail.com>
2018-11-14 18:47:44 +0100

    Primer Commit

---
+++ JFXsignupFondo!/src/application/Main.java
@@ -0,0 +1,31 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("interfaz.fxml"));
+
+			BorderPane root = new BorderPane();
+			AnchorPane pane = loader.load();
+			Scene scene = new Scene(pane);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setTitle("");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
